e9faf52e9100a642ad604dfb90b893ea0db79561
dittp2 <pascal.dittli@outlook.com>
2018-01-02 11:29:27 +0100

    readxml and convertJson

--- GlucoMan/src/pages/epd/conv.js
+++ GlucoMan/src/pages/epd/conv.js
@@ -1,10 +1,7 @@
 var parseString = require('xml2js').parseString;
 
 
-exports.test = function() {
-   // var xmlData =  `<?xml version=1.0?>
-   // <Company root=\"1.3.6.1.4.1.19376.1.5.3.1.1.1\"><Employee><FirstName>Tanmay</FirstName><LastName>Patil</LastName><ContactNo>1234567890</ContactNo><Email>tanmaypatil@xyz.com</Email><Address><City>Bangalore</City><State>Karnataka</State><Zip>560212</Zip></Address></Employee></Company>`;
-    
+exports.convXMLToJson = function() {
     var xml = new XMLHttpRequest();
     xml.open('GET', 'assets/data/junior.xml', false);
     xml.send();
@@ -12,10 +9,16 @@ exports.test = function() {
 
     parseString(xmlData, function (err, result) {
     console.dir(result);
+    var stringifiedResult = JSON.stringify(result);
+    //document.write(stringifiedResult);
+    document.getElementById('output').innerHTML = stringifiedResult;
+    //document.write(result)
+    //document.getElementById('output').nodeValue = result;
+    //document.getElementById('output').innerHTML = result;
+    
     });
 }
 
-//exports.test = test;
 
 
 
--- GlucoMan/src/pages/epd/epd.ts
+++ GlucoMan/src/pages/epd/epd.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
 import { xml2js } from 'xml2js';
 import { Http } from '@angular/http';
 import { NavController } from 'ionic-angular/navigation/nav-controller';
-import * as test from '../epd/conv.js';
+import * as convXMLToJson from '../epd/conv.js';
 //import { toJson } from 'xml2json';
 //import * as xml2js from "xml2js";
 
@@ -69,8 +69,10 @@ export class EpdPage {
     xml.open('GET', 'assets/data/junior.xml', false);
     xml.send();
     var xmlData = xml.responseText;
-    document.write(xmlData);
+    //document.write(xmlData);
+    document.getElementById('output').innerHTML = xmlData;
     this.xmlDatapass = xmlData;
+
   }
   
 
@@ -85,68 +87,13 @@ export class EpdPage {
     });
   }
 
- test(){
-   test.test();
+  convXMLToJson(){
+    var result = convXMLToJson.convXMLToJson();
+    return result; 
  }
 }
 
 /*
-  //read local xml file and Write in document
-  readXML() {
-    var xml = new XMLHttpRequest();
-    var xml2js = new xml2js();
-    xml.open('GET', 'assets/data/junior.xml', false);
-    xml.send();
-    var xmlData = xml.responseXML;
-    var xmlData = xmlData.toS
-    var jsonData = toJson(xmlData);
-    document.write(jsonData);
-
-    //var jsonData = converter(xmlData);
-    //var beautifiedJson = JSON.stringify(jsonData, undefined, 4);
-    //document.getElementById('jsonData').innerHTML = beautifiedJson;
-  }
-
-
-  myjson(){
-    var req = this.http.get("my_link_for_xml");
-    return req.map((res:Response) => {
-    let c = res.text();
-    let myJson = xml2js.xml_str2json(c);
-    return myJson;
-    });
-    }
-
-}
-
-
-
-
-/*
-converter(data: string): Object {
-
-  let res;
-  xml2js.parseString(data, { explicitArray: false});
-  
-  }
-
-changeFile(){
-  document.write(this.converter('assets/data/junior.xml'));
-}
-
-
-  //convert various xml into json 
-  convertXMLToJson() {
-    //return Promise.resolve(['This', 'That']);
-    var xml = "<root>Hello xml2js!</root>"
-    xml2js.parseString(xml);
-    alert("parsing war erfolgreich!");
-  }
-
-
-
-
-
 Funktioniert
 readXML() {
     var xml = new XMLHttpRequest();
@@ -154,126 +101,5 @@ readXML() {
     xml.send();
     var xmlData = xml.responseText;
     document.write(xmlData);
-  }
-
-      document.write("<table border='1'>");
-    var x = xmlData.getElementsByTagName("")
-    var i = 0;
-    for (i = 0;i < x.length;i++)
-    {
-      document.write("<tr><td>");
-      document.write(x[i].getElementsByTagName("name")[0].childNodes[0].nodeValue);
-      document.write("</tr>");
-      document.write("</table>");
-    }
-
-
-
-  Do(target: string) {
-    switch (target) {
-      case 'XML':
-        break;
-      case 'JSON':
-        break;
-    }
-  }
-
-
-var Connect = new XMLHttpRequest();
-Connect.open("GET", "assets/data/junior.xml", false);
-Connect.setRequestHeader("Content-Type", "text/xml")
-Connect.send(null);
-
-var TheDocument = Connect.responseXML;
-var Patient = TheDocument.childNodes[0];
-for(var i = 0; i < Patient.children.length; i++)
-{
-  var Patient = Patient.children[i];
 }
-
-
-
-
-
-  public getRandomUser() {
-    return this
-      .http
-      .get('https://randomuser.me/api/?format=xml', { responseType: "text" })      
-      // use the pipe method
-      .pipe(
-        // ... to apply oprtators to obervables
-        map((res: string) => this.transformProvider.convertToJson(res))
-      )
-      .subscribe((res: Object) => {
-        // do whatever you wanna do with your data
-        console.dir(res);
-      });
-  }
-
-
-  GetXML() {
-    this.http.get('assets/data/junior.xml').subscribe(data => {
-        this.posts = JSON.stringify(data);
-        console.log(typeof(this.posts));
-        xml2js.parseString(this.posts, function (err, result) {
-            console.log(result);
-        });
-    }, error => {
-        console.log(JSON.stringify(error.json()));
-    });
-}
-
-
-
-ionViewWillEnter()
-{
-   this.loadXML();
-}
-
-loadXML()
-   {
-      this.http.get('/assets/data/junior.xml')
-      .map(res => res.text())
-      .subscribe((data)=>
-      {
-         this.parseXML(data)
-         .then((data)=>
-         {
-            this.xmlItems = data;
-         });
-      });
-   }
-
-   parseXML(data)
-   {
-      return new Promise(resolve =>
-      {
-         var k,
-             arr    = [],
-             parser = new xml2js.Parser(
-             {
-                trim: true,
-                explicitArray: true
-             });
-
-         parser.parseString(data, function (err, result)
-         {
-            var obj = result.comics;
-            for(k in obj.publication)
-            {
-               var item = obj.publication[k];
-               arr.push({
-                  id           : item.id[0],
-                  title        : item.title[0],
-                  publisher : item.publisher[0],
-                  genre        : item.genre[0]
-               });
-            }
-
-            resolve(arr);
-         });
-      });
-   }
-   */
-
-
+*/
